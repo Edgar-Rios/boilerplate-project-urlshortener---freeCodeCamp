a88f5594b70264c8c15ede15ba817cc8e1565085
Edgar-Rios <sistunla2020@gmail.com>
2022-09-04 10:00:20 -0300

    proyect ready

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +19,46 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MI CODIGO
+const {read: data, write} = require('./store');
+
+app.post('/api/shorturl', (req, res) => {
+
+  const {url} = req.body;
+
+  if(!url.includes('http://'))
+    return res.json({ "error": 'Invalid url'});
+  
+  let newSite = write({original_url: url});
+
+  res.json(newSite);
+
+})
+
+app.get('/api/shorturl/:idUrl', (req, res) => {
+try {
+  const { idUrl } = req.params;
+
+  if(isNaN(idUrl))  throw Error("Wrong format");
+  // console.log(data)
+  let url;
+  data.forEach(({short_url, original_url }) => {
+    if(short_url == idUrl)
+      url = original_url;
+  })
+  
+  if(!url) throw Error("No short short_url found for the given input")
+
+  
+  return res.redirect(url)
+} catch (err){
+  res.json({error: err.message})
+}
+})
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ MI CODIGO
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
---
+++ store/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const route = path.join(__dirname, 'webSites.json');
+const data = JSON.parse(fs.readFileSync(route, "utf-8"));
+
+function lastId() {
+    if(data.length == 0) return 1;
+    console.log(data)
+    let preId = 0;
+    data.forEach(({short_url}) => {
+        if(short_url>preId)
+            preId = short_url;
+    });
+    return preId+1;
+}
+
+
+exports.read = data;
+exports.write = (newData) => {
+    newData.short_url = lastId();
+    data.push(newData);
+    fs.writeFileSync(route, JSON.stringify(data), "utf-8");
+    
+    return newData;
+}
+
